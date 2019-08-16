2d261311424296d74623f8ff61d5a80cb2765381
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2019-08-16 14:53:56 +0200

    License headers and other legal stuff

--- logback-ecs-encoder/src/main/java/co/elastic/logging/logback/EcsEncoder.java
+++ logback-ecs-encoder/src/main/java/co/elastic/logging/logback/EcsEncoder.java
@@ -1,3 +1,27 @@
+/*-
+ * #%L
+ * Java ECS logging
+ * %%
+ * Copyright (C) 2019 Elastic and contributors
+ * %%
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * #L%
+ */
 package co.elastic.logging.logback;
 
 import ch.qos.logback.classic.pattern.ThrowableProxyConverter;
@@ -6,15 +30,16 @@ import ch.qos.logback.core.encoder.EncoderBase;
 import co.elastic.logging.EcsJsonSerializer;
 import co.elastic.logging.JsonUtils;
 
-import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset;
 import java.util.HashSet;
 import java.util.Set;
 
 public class EcsEncoder extends EncoderBase<ILoggingEvent> {
 
+    private static final Charset UTF_8 = Charset.forName("UTF-8");
     private String serviceName;
     private ThrowableProxyConverter throwableProxyConverter;
-    private Set<String> topLevelLabels = new HashSet<>(EcsJsonSerializer.DEFAULT_TOP_LEVEL_LABELS);
+    private Set<String> topLevelLabels = new HashSet<String>(EcsJsonSerializer.DEFAULT_TOP_LEVEL_LABELS);
 
     @Override
     public byte[] headerBytes() {
@@ -41,7 +66,7 @@ public class EcsEncoder extends EncoderBase<ILoggingEvent> {
         EcsJsonSerializer.serializeLabels(builder, event.getMDCPropertyMap(), topLevelLabels);
         EcsJsonSerializer.serializeObjectEnd(builder);
         // all these allocations kinda hurt
-        return builder.toString().getBytes(StandardCharsets.UTF_8);
+        return builder.toString().getBytes(UTF_8);
     }
 
     private void serializeException(ILoggingEvent event, StringBuilder builder) {
