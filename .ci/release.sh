537cd36df3da3b46ac3bc48ad2f357433c140ecb
Adrien Mannocci <adrien.mannocci@elastic.co>
2024-02-02 11:59:08 +0100

    feat: new release process (#226)
    * feat: new release process
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * feat: use a global maven config file
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * chore: execute package goal for dry-run release
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * ci: align pr title creation for release
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * ci: add parameter documentations in CI scripts
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * ci: centralize maven goal execution
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * fix: use directly java-version without file
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * ci: refactor using a reusable workflow
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * refactor: avoid duplicate hardcode branch name
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * ci: create summary file in any case
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * feat: validate tag before anything
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * chore: apply suggestions
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * fix: validate correctly tags
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * chore: add validation for tag and project version
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    ---------
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>

--- .ci/release.sh
+++ .ci/release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ##  This script runs the release given the different environment variables
-##    branch_specifier
-##    dry_run
+##    ref      : git reference (commit,branch, tag, ...)
+##    dry_run  : dry-run when set to 'true'
 ##
 ##  It relies on the .buildkite/hooks/pre-command so the Vault and other tooling
 ##  are prepared automatically by buildkite.
@@ -19,7 +19,7 @@ clean_up () {
 trap clean_up EXIT
 
 # Avoid detached HEAD since the release plugin requires to be on a branch
-git checkout -f "${branch_specifier}"
+git checkout -f "${ref}"
 
 echo "--- Debug JDK installation :coffee:"
 echo $JAVA_HOME
@@ -28,9 +28,8 @@ java -version
 
 set +x
 echo "--- Release the binaries to Maven Central :maven:"
-if [[ "$dry_run" == "true" ]] ; then
-  echo './mvnw -V release:prepare release:perform --settings .ci/settings.xml --batch-mode'
+if [[ "${dry_run}" == "true" ]] ; then
+  ./mvnw -V -s .ci/settings.xml -Pgpg clean package --batch-mode | tee release.txt
 else
-  # providing settings in arguments to make sure they are propagated to the forked maven release process
-  ./mvnw -V release:prepare release:perform --settings .ci/settings.xml -Darguments="--settings .ci/settings.xml" --batch-mode | tee release.txt
+  ./mvnw -V -s .ci/settings.xml -Pgpg clean deploy --batch-mode | tee release.txt
 fi
