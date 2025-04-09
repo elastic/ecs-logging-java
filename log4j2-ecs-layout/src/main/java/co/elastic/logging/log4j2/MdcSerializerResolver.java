84d340a184e47bd7297fae1611c2a6575815b80c
jackshirazi <jack.shirazi@elastic.co>
2025-04-09 13:14:49 +0100

    Update MdcSerializerResolver.java

--- log4j2-ecs-layout/src/main/java/co/elastic/logging/log4j2/MdcSerializerResolver.java
+++ log4j2-ecs-layout/src/main/java/co/elastic/logging/log4j2/MdcSerializerResolver.java
@@ -40,7 +40,7 @@ class MdcSerializerResolver {
             Class<?> clazz = Class.forName(mdcSerializerFullClassName);
             return (MdcSerializer) clazz.getDeclaredConstructor().newInstance();
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException |
-                 InvocationTargetException e) {
+                 InvocationTargetException e) { //build failure?
             throw new IllegalArgumentException("Could not create MdcSerializer " + mdcSerializerFullClassName, e);
         }
     }
